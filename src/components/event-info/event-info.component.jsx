3db771a9acbe6795b9d4a558fd9f59705c2d7772
PageCreativeStudio <113024112+PageCreativeStudio@users.noreply.github.com>
2024-01-25 15:11:11 +0000

    Update event-info.component.jsx

--- src/components/event-info/event-info.component.jsx
+++ src/components/event-info/event-info.component.jsx
@@ -127,7 +127,7 @@ END:VCALENDAR
       );
   
       // Open the download link in a new window for mobile screens
-      window.open(downloadLink, '_blank');
+      window.open(downloadLink);
     } else {
       // Create an anchor element to trigger the download for desktop screens
       const downloadLink = document.createElement('a');
