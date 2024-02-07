f804831dfb86f98012f081a019775d61bea2353a
PageCreativeStudio <113024112+PageCreativeStudio@users.noreply.github.com>
2024-02-07 15:41:03 +0000

    Update event-info.component.jsx

--- src/components/event-info/event-info.component.jsx
+++ src/components/event-info/event-info.component.jsx
@@ -108,32 +108,34 @@ END:VCALENDAR
       endDateStringWithoutTime = startDateStringWithoutTime;
     }
 
-    const filename = `${title}.ics`;
+  const filename = `${title}.ics`;
 
-    const calendarDataUrl = generateCalendarData(
-      new Date(startDateStringWithoutTime),
-      new Date(endDateStringWithoutTime),
-      time,
-      timeEnd
-    );
-  
-    // For mobile, open a new blank tab using the 'window.location.href' method
-    if (window.innerWidth <= 767) {
-      window.location.href = calendarDataUrl;
-    } else {
-      // For desktop, trigger the click and remove the link
-      const downloadLink = document.createElement('a');
-      downloadLink.href = calendarDataUrl;
-      downloadLink.download = filename;
-      document.body.appendChild(downloadLink);
-      downloadLink.click();
-      document.body.removeChild(downloadLink);
-  
-      // Cleanup: revoke the object URL after a short delay
-      setTimeout(() => {
-        window.URL.revokeObjectURL(calendarDataUrl);
-      }, 1000);
-    }
+  const calendarDataUrl = generateCalendarData(
+    new Date(startDateStringWithoutTime),
+    new Date(endDateStringWithoutTime),
+    time,
+    timeEnd
+  );
+
+  // For mobile, open a new blank tab using the 'window.open' method
+  if (window.innerWidth <= 767) {
+    const newWindow = window.open();
+    newWindow.document.write(`<a href="${calendarDataUrl}" download="${filename}">Download iCal</a>`);
+    newWindow.document.close();
+  } else {
+    // For desktop, trigger the click and remove the link
+    const downloadLink = document.createElement('a');
+    downloadLink.href = calendarDataUrl;
+    downloadLink.download = filename;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+
+    // Cleanup: revoke the object URL after a short delay
+    setTimeout(() => {
+      window.URL.revokeObjectURL(calendarDataUrl);
+    }, 1000);
+  }
   };
 
 
