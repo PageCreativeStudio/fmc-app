c9153d92dbc1806e607a394442ff23c20e1f553d
PageCreativeStudio <113024112+PageCreativeStudio@users.noreply.github.com>
2024-10-29 09:28:00 +0000

    Update event-info.component.jsx

--- src/components/event-info/event-info.component.jsx
+++ src/components/event-info/event-info.component.jsx
@@ -5,36 +5,39 @@ import { withTheme } from "@emotion/react";
 
 const EventInfo = ({ theme, title, date, dateEnd, time, timeEnd, description, image, colour = theme.colors.primary, onClick }) => {
 
-  // Format date and time to ICS format
   const formatICSDate = (date, time) => {
     const dateObj = new Date(date);
+  
     if (time) {
       const timeParts = time.match(/(\d+):(\d+)\s*([ap]m)/i);
       if (timeParts) {
         let hours = parseInt(timeParts[1], 10);
         const minutes = parseInt(timeParts[2], 10);
         const period = timeParts[3].toLowerCase();
-        if (period === 'pm' && hours !== 12) hours += 12;
+        if (period === "pm" && hours !== 12) hours += 12;
         dateObj.setHours(hours, minutes);
+      } else {
+        console.error(`Failed to parse time: ${time}`);
       }
     } else {
+      // Set to 00:00 local time if no time is provided
       dateObj.setHours(0, 0, 0, 0);
     }
-
+  
     const year = dateObj.getFullYear();
     const month = (dateObj.getMonth() + 1).toString().padStart(2, '0');
     const day = dateObj.getDate().toString().padStart(2, '0');
     const hours = dateObj.getHours().toString().padStart(2, '0');
     const minutes = dateObj.getMinutes().toString().padStart(2, '0');
     const seconds = dateObj.getSeconds().toString().padStart(2, '0');
-
+    
     return `${year}${month}${day}T${hours}${minutes}${seconds}`;
   };
 
   // Generate ICS file for a single event
   const generateCalendarData = (startDate, endDate, startTime, endTime, title, description) => {
     const formattedStartDate = formatICSDate(startDate, startTime);
-    const formattedEndDate = formatICSDate(endDate || startDate, endTime || startTime); // Use start time if end time is not provided
+    const formattedEndDate = formatICSDate(endDate || startDate, endTime || startTime);
 
     const calendarData = `
 BEGIN:VCALENDAR
@@ -52,11 +55,55 @@ END:VCALENDAR`.trim();
     return window.URL.createObjectURL(blob);
   };
 
-  // Handle download for each event
-  const handleDownload = (event) => {
-    const { date_from, date_to, time, time_end, title, description } = event.acf;
-    const calendarDataUrl = generateCalendarData(date_from, date_to, time, time_end, title, description);
-    window.open(calendarDataUrl, '_blank');
+  const handleDownload = () => {
+    const startDateString = date ? date.split(" ") : [];
+    const endDateString = dateEnd ? dateEnd.split(" ") : [];
+    const months = {
+      January: '01',
+      February: '02',
+      March: '03',
+      April: '04',
+      May: '05',
+      June: '06',
+      July: '07',
+      August: '08',
+      September: '09',
+      October: '10',
+      November: '11',
+      December: '12',
+    };
+
+    let startDateStringWithoutTime;
+    let endDateStringWithoutTime;
+
+    if (startDateString.length === 4) {
+      startDateStringWithoutTime = `${startDateString[3]}-${months[startDateString[2]]}-${startDateString[1].slice(0, -2)}`;
+    } else if (startDateString.length === 5) {
+      const startTime = startDateString[4].split(":");
+      startDateStringWithoutTime = `${startDateString[3]}-${months[startDateString[2]]}-${startDateString[1].slice(0, -2)}T${startTime[0].padStart(2, '0')}${startTime[1].padStart(2, '0')}`;
+    }
+
+    if (endDateString.length === 4) {
+      endDateStringWithoutTime = `${endDateString[3]}-${months[endDateString[2]]}-${endDateString[1].slice(0, -2)}`;
+    } else if (endDateString.length === 5) {
+      const endTime = endDateString[4].split(":");
+      endDateStringWithoutTime = `${endDateString[3]}-${months[endDateString[2]]}-${endDateString[1].slice(0, -2)}T${endTime[0].padStart(2, '0')}${endTime[1].padStart(2, '0')}`;
+    } else {
+      console.warn("No end date provided for event. Using start date as end date.");
+      endDateStringWithoutTime = startDateStringWithoutTime;
+    }
+
+    if (endDateStringWithoutTime) {
+      const calendarDataUrl = generateCalendarData(
+        new Date(startDateStringWithoutTime),
+        new Date(endDateStringWithoutTime),
+        time,
+        timeEnd
+      );
+      window.open(calendarDataUrl, '_blank');
+    } else {
+      console.warn("No end date provided for event.");
+    }
   };
 
   return (
